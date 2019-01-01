086db165aa75888abba00229eecfc415dce55a95
Zackary Hall <hallzac2@isu.edu>
2019-01-01 16:16:33 -0700

    I took a first pass at implementing a bubble sort with comparator

--- src/main/java/zhall/sorting/algorithm/sampler/BubbleSort.java
+++ src/main/java/zhall/sorting/algorithm/sampler/BubbleSort.java
@@ -11,7 +11,16 @@ public class BubbleSort {
     private BubbleSort() {
     }
 
+    // TODO check inputs for errors
     public static <T> void sort(Comparator<T> comparator, T[] array) {
-        // TODO
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (comparator.compare(array[j], array[j + 1]) == 1) {
+                    T temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
     }
 }
