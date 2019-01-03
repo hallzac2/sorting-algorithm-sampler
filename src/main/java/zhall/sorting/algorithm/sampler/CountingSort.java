06bcda9fd5a5402e041973c29c17b519742521ab
zhall <zhall@clearwateranalytics.com>
2019-01-03 14:07:36 -0700

    Convert to counting sort and added default method for Comparable arrays

--- src/main/java/zhall/sorting/algorithm/sampler/CountingSort.java
+++ src/main/java/zhall/sorting/algorithm/sampler/CountingSort.java
@@ -6,9 +6,13 @@ import java.util.Comparator;
  *
  * @author zhall
  */
-public class BucketSort {
+public class CountingSort {
 
-    private BucketSort() {
+    private CountingSort() {
+    }
+    
+    public static <T extends Comparable> void sort(T[] array) {
+        sort(T::compareTo, array);
     }
 
     public static <T> void sort(Comparator<T> comparator, T[] array) {
